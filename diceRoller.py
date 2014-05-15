90e5f00eabe6f99205f283285e5de904820664dc
Jams <jaykin7072@sohamcollege.org.uk>
2014-05-15 09:48:49 +0100

    Update diceRoller.py
    swaaaggg

--- diceRoller.py
+++ diceRoller.py
@@ -1,7 +1,7 @@
-#Coin flip program
+#Dice roll program.
 #Describe the purpose of this program here.
 
-inport random;time
+import random,time
 
 s1 = "- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n"
 s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"
@@ -10,26 +10,60 @@ s4 = "- - - - -\n| O   O |\n|       |\n| O   O |\n- - - - -\n"
 s5 = "- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n"
 s6 = "- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"
 
-def roll():
-    Print("rolling....."
-    roll = rand.randing(7)
+def rollDice():
+    print("Rolling.....")
+    roll = random.randint(1,6)
+    return roll
 
 
-def show_dice(roll)
-    if roll = 1:
-        print(S1)
-    else roll = 2
+def show_dice(roll):
+    if roll == 1:
+        print(s1)
+    elif roll == 2:
         print(s2)
-    else roll = 3
+    elif roll == 3:
         print(s3)
-    else roll = 4
+    elif roll == 4:
         print(s4)
-    else roll = 5
+    elif roll == 5:
         print(s5)
-    else roll = 6
+    elif roll == 6:
         print(s6)
+    else:
+        print("Something is wrong with this code.")
+        print(roll)
 
-roll
-time.sleep(1)
-show_dice(roll)
+def twoInRow():
+    howManyRolls=int(input("How many times would you like to roll? > "))
+    prevRoll="0"
+    for x in range(howManyRolls):
+        myRoll=rollDice()
+        time.sleep(1)
+        show_dice(myRoll)
+        if prevRoll==myRoll:
+            print("You win life. Grats.")
+            break
+        prevRoll=myRoll
 
+def whenSix():
+    howManyRolls=int(input("How many times would you like to roll? > "))
+    prevRoll="0"
+    for x in range(howManyRolls):
+        myRoll=rollDice()
+        time.sleep(1)
+        show_dice(myRoll)
+        if myRoll=="6":
+            print("You win life. Grats")
+            break
+
+whichGame=str(input("Would you like it to stop at 2 in a row (t), \nOr when you get a six (s)? > "))
+if whichGame=="t":
+    twoInRow()
+elif whichGame=="s":
+    whenSix()   
+
+
+    
+
+#All of the elses except the last one needed to be elif and they all needed
+#== and : at the end. The show_dice function was the most buggy.
